34235c09c24a0175fcafbfe4a16bea2bc439cad0
404 rererererelo <yunjongha06@gmail.com>
2024-10-01 10:12:48 +0900

    (아직 작업중. 디버깅 다 안돌려봄) Move와 Coord는 Board를 구현하기 위한 기본 클래스라고 생각하면 된다. Board는 8*8짜리 구조체를 클래스화 해서 몇몇 기능을 추가한 것이다. 기능이라 하면, 특정 위치에 존재하는 말을 반환하는 것과, 특정 위치의 말이 움직일 수 있는 경우의 수를 구하는 것이다.

--- Pruning/pruning_tree.cpp
+++ Pruning/pruning_tree.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include "Board.h"
 
 int main(void) {
-	printf("Hello world!");
-	printf("TEST2");
+	printf("Hello world!\n");
+	printf("TEST2\n");
 	return 0;
+
+
+
+	string fen = "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1";
+	Board b = Board(fen);
+	b.print();
+
+	Board a = b;
+	a.print();
+
+	b.set_on(Coord(RANK_8, FILE_A), ' ');
+	a.print();
 }
