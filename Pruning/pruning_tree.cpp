62be1d5a3e67a9a23f56af9fd4095010b7782440
404 rererererelo <yunjongha06@gmail.com>
2024-10-01 16:02:16 +0900

    모든 기물의 가능한 움직임을 계산하는 함수 완성. 개발의 편의 때문에 지금은 Board->vector<Board>지만, 메모리 효율을 위해 Move->vector<Move>로 고칠 예정.

--- Pruning/pruning_tree.cpp
+++ Pruning/pruning_tree.cpp
@@ -4,17 +4,18 @@
 int main(void) {
 	printf("Hello world!\n");
 	printf("TEST2\n");
+
+
+
+	//string fen = "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1 - - - - -";
+	string fen = "8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1";
+	Board a = Board(fen);
+	a.print();
+	std::cout << string(32, ' ') << '\n';
+
+	vector<Board> boards = a.movable_boards(WHITE);
+	for (const Board& board : boards)
+		board.print();
+
 	return 0;
-
-
-
-	string fen = "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1";
-	Board b = Board(fen);
-	b.print();
-
-	Board a = b;
-	a.print();
-
-	b.set_on(Coord(RANK_8, FILE_A), ' ');
-	a.print();
 }
